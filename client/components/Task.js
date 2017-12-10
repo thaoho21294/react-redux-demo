1b29738f073ef25058407c188f5f66b80232a3ee
thaoho21294 <thaoho21294@gmail.com>
2017-12-10 22:41:45 +0700

    refactor code

--- client/components/Task.js
+++ client/components/Task.js
@@ -8,11 +8,19 @@ const Task = (props) => {
   return (
     <div className="row">
       <div className="btn-group" role="group" aria-label="Basic example">
-        <Button type="button" onClick={() => {
-          props.putChangeStatus(props.Obj, props.isComplete)}} className="btn" color="primary">{props.isComplete ? "Undo" : "Complete" }</Button>
-        <Button type="button" onClick={() => props.deleteTask(props.Obj.slug)} className="btn" color="danger">Delete</Button>
+        <Button
+          type="button"
+          onClick={() => {
+            props.putChangeStatus(props.Obj, props.isComplete)
+          }}
+          className="btn"
+          color="primary"
+        >
+          {props.isComplete ? 'Undo' : 'Complete' }
+        </Button>
+        <Button type="button" onClick={() => { props.deleteTask(props.Obj.slug); }} className="btn" color="danger">Delete</Button>
       </div>
-      <h3 style={{textDecoration: props.isComplete ? "line-through" : "none"}}>{props.name}</h3>
+      <h3 style={{ textDecoration: props.isComplete ? 'line-through' : 'none' }}>{props.name}</h3>
     </div>
   );
 };
--- client/scenes/Home.js
+++ client/scenes/Home.js
@@ -3,9 +3,9 @@ import { connect } from 'react-redux';
 import { Container, Row, Col } from 'reactstrap';
 
 import { getAllTasks } from '../redux/reducer';
-// import Task from '../components/Task';
-// import AddTaskForm from '../components/AddTaskForm';
-// import LefMenu from '../components/LeftMenu';
+import Task from '../components/Task';
+import AddTaskForm from '../components/AddTaskForm';
+import LefMenu from '../components/LeftMenu';
 
 class Home extends Component {
   componentDidMount() {
@@ -17,10 +17,23 @@ class Home extends Component {
         <Container>
           <Row>
             <Col xs="6">
-              LefMenu
+              <LefMenu />
             </Col>
             <Col xs="6">
-              abc ddddd
+              <div className="container">
+                <h1>Cosmic To-Do App!!</h1>
+                <AddTaskForm />
+              </div>
+              <h3>Let's get some work done!</h3>
+              <div className="container">
+                {
+                  this.props.tasks && this.props.tasks.map((task) => {
+                    return (
+                      <Task key={task._id} Obj={task} isComplete={task.metafields[0].value} name={task.title} />
+                    );
+                  })
+                }
+              </div>
             </Col>
           </Row>
         </Container>
