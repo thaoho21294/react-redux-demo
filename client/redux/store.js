8e45846d814d1c2d6b51159d8b5aa012a12f558c
thaoho21294 <thaoho21294@gmail.com>
2020-04-19 15:29:22 +0700

    Remove unecessary dependencies

--- client/redux/store.js
+++ client/redux/store.js
@@ -1,16 +1,10 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import { createLogger } from 'redux-logger';
+import { createStore } from 'redux';
 import reducer from './reducers/index';
 
 /* eslint-disable no-underscore-dangle */
 const store = createStore(
   reducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-  applyMiddleware(
-    createLogger(),
-    thunkMiddleware,
-  ),
 );
 
 export default store;
