487255c18b03085e95be8f50e1a13e134cf111aa
thaoho21294 <thaoho21294@gmail.com>
2020-04-19 13:24:38 +0700

    Move logic adding a task to an effect

--- client/components/AddTaskForm.js
+++ client/components/AddTaskForm.js
@@ -1,33 +1,31 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addTask } from '../redux/reducer';
-import { postTaskApi } from '../service/tasksService';
+import { postTaskEffect } from '../redux/effect';
 
 function AddTaskForm() {
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState('');
   const dispatch = useDispatch();
 
   async function onSubmit(evt) {
-    setLoading(true);
     evt.preventDefault();
     const title = evt.target.taskName.value;
-    const response = await postTaskApi(title);
-    setLoading(false);
-    dispatch(addTask(response));
-  }
-
-  if (loading) {
-    return 'loading';
+    await postTaskEffect({ title, dispatch, setLoading, setError });
   }
 
   return (
-    <form onSubmit={onSubmit}>
-      <div className="form-group">
-        <label htmlFor="taskName1">Add New To-Do</label>
-        <input id="taskName1" name="taskName" placeholder="Enter new task" />
-      </div>
-      <button type="submit">Add</button>
-    </form>);
+    <React.Fragment>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="taskName1">Add New To-Do</label>
+          <input id="taskName1" name="taskName" placeholder="Enter new task" />
+        </div>
+        <button type="submit">Add</button>
+      </form>
+      { error && <div className="text-danger">error</div> }
+      { loading && <div className="text-info">loading...</div> }
+    </React.Fragment>
+  );
 }
 
 export default React.memo(AddTaskForm);
--- client/redux/effect.js
+++ client/redux/effect.js
@@ -1,11 +1,24 @@
-import { getTasks } from './reducer';
-import { getTasksApi } from '../service/tasksService';
+import { getTasks, addTask } from './actions';
+import { getTasksApi, postTaskApi } from '../service/tasksService';
 
-export default async function fetchTasksFromAPI({ dispatch, setError, setLoading }) {
+export async function fetchTasksEffect({ dispatch, setError, setLoading }) {
   try {
     const response = await getTasksApi();
     const json = await response.json();
     dispatch(getTasks(json));
+  } catch (e) {
+    setError(e.message || 'Unexpected Error!!!');
+  }
+  setLoading(false);
+}
+
+export async function postTaskEffect({ title, dispatch, setError, setLoading }) {
+  setLoading(true);
+  try {
+    // https://developer.mozilla.org/en-US/docs/Web/API/Response
+    const response = await postTaskApi(title);
+    const newTask = await response.json();
+    dispatch(addTask(newTask));
   } catch (e) {
     setError(e.message || 'Unexpected Error!!!');
   }
