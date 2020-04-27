cf72b9b5f7d55ceff07cd914501056fcaf986563
thaoho21294 <thaoho21294@gmail.com>
2020-04-27 10:36:11 +0700

    Refactor Main View useState to useReducer

--- client/scenes/MainView.js
+++ client/scenes/MainView.js
@@ -1,39 +1,41 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { VIEW_TYPE } from '../constant';
+import { VIEW_TYPE, TASK_STATUS } from '../constant';
 import AllTasksView from './AllTasksView';
 import CompletedTaskView from './CompletedTasksView';
 import TodoTasks from './TodoTasksView';
 import { fetchTasksEffect } from '../redux/effect';
 
 export default function MainView() {
-  const tasks = useSelector(state => state.taskList.tasks);
-  const view = useSelector(state => state.view.currentView);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState('');
+  const tasks = useSelector(state => state.tasks);
+  const view = useSelector(state => state.view);
+  const [state, setState] = React.useReducer((s, a) => ({ ...s, ...a }), {
+    loading: true,
+    error: null,
+  });
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    fetchTasksEffect({ dispatch, setError, setLoading });
+  React.useEffect(() => {
+    fetchTasksEffect(dispatch, setState);
   }, []);
 
-  if (loading) {
+  if (state.loading) {
     return <div>Loading</div>;
   }
 
-  if (error) {
-    return <div className="text-danger">ERROR: {error}</div>;
+  if (state.error) {
+    return <div className="text-danger">ERROR: {state.error}</div>;
   }
 
   switch (view) {
     case VIEW_TYPE.ALL_TASK: return <AllTasksView tasks={tasks} />;
     case VIEW_TYPE.COMPLETED_TASK: {
-      const completedTasks = tasks.filter(task => task.isCompleted);
+      const completedTasks = tasks.filter(task => task.status === TASK_STATUS.COMPLETED);
       return <CompletedTaskView tasks={completedTasks} />;
     }
     case VIEW_TYPE.TODO_TASK: {
-      const todoTasks = tasks.filter(task => !task.isCompleted);
+      const todoTasks = tasks.filter(task => task.status === TASK_STATUS.TODO);
       return <TodoTasks tasks={todoTasks} />;
     }
     default: return '';
