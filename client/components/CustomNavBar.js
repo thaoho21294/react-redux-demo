ed501ca9c34be30abd673aa634aa5fa937ffa482
thaoho21294 <thaoho21294@gmail.com>
2020-05-13 12:59:14 +0700

    Use context to get data from parrent

--- client/components/CustomNavBar.js
+++ client/components/CustomNavBar.js
@@ -1,18 +1,25 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Navbar, Form, FormControl, Button } from 'react-bootstrap';
+import { useUserState } from '../scenes/Home';
 
 function CustomNavBar() {
+  const { name } = useUserState();
   return (
     <Navbar bg="light" expand="lg" >
       <Navbar.Brand href="#home" className="mr-auto">
         <FontAwesomeIcon icon={['fas', 'tasks']} />
         Todo App
       </Navbar.Brand>
-      <Form inline>
-        <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-        <Button variant="success">Search</Button>
-      </Form>
+      <Navbar.Collapse className="justify-content-end">
+        <Form inline>
+          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+          <Button variant="success">Search</Button>
+        </Form>
+        <Navbar.Text>
+          Signed in as: <a href="#login">{ name }</a>
+        </Navbar.Text>
+      </Navbar.Collapse>
     </Navbar>
   );
 }
--- client/scenes/Home.js
+++ client/scenes/Home.js
@@ -7,18 +7,61 @@ import Style from '../styles/home.module.scss';
 import MainView from './MainView';
 import ErrorBoundary from '../components/ErrorBoundary';
 
+const UserContext = React.createContext();
+const UserDispatchContext = React.createContext();
+
+const defaultUser = { id: 'u00001', name: 'John', email: 'john@gmail.com' };
+
+function userReducer(state, action) {
+  switch (action.type) {
+    case 'get': {
+      return state;
+    }
+    case 'update': {
+      return { ...state, ...action.user };
+    }
+    default: {
+      throw new Error(`Unhandled action type: ${action.type}`);
+    }
+  }
+}
+
 export default function Home() {
+  const [state, dispatch] = React.useReducer(userReducer, defaultUser);
+
   return (
-    <div>
-      <CustomNavBar />
-      <div className={classnames(Style.content, 'container')}>
-        <LefMenu />
-        <div className={Style.tasks}>
-          <ErrorBoundary>
-            <MainView />
-          </ErrorBoundary>
+    <UserContext.Provider value={state}>
+      <UserDispatchContext.Provider value={dispatch}>
+        <CustomNavBar />
+        <div className={classnames(Style.content, 'container')}>
+          <LefMenu />
+          <div className={Style.tasks}>
+            <ErrorBoundary>
+              <MainView />
+            </ErrorBoundary>
+          </div>
         </div>
-      </div>
-    </div>
+      </UserDispatchContext.Provider>
+    </UserContext.Provider>
   );
 }
+
+function useUserState() {
+  const context = React.useContext(UserContext);
+  if (context === undefined) {
+    throw new Error('useUserState must be used within a UserProvider');
+  }
+
+  return context;
+}
+
+function useUserDispatch() {
+  const context = React.useContext(UserDispatchContext);
+  if (context === undefined) {
+    throw new Error('useUserDispatch must be used within a UserProvider');
+  }
+
+  return context;
+}
+
+export { useUserState, useUserDispatch };
