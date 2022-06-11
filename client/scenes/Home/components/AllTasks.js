ee27cf9d8f9238df1621b84ffadd56b0210c157e
Thao <thaoho21294@gmail.com>
2022-06-11 21:56:15 +0700

    Fix tasks changed but filtered tasks doesn't change and remove debounce

--- client/scenes/Home/components/AllTasks.js
+++ client/scenes/Home/components/AllTasks.js
@@ -1,23 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useMemo, useRef, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { FormControl } from 'react-bootstrap';
 import TasksView from './TasksView';
 import { tasksSelector } from '../../../redux/task/task.selector';
-import useDebounce from '../../../hooks/useDebounce';
 
 const AllTasks = () => {
   const tasks = useSelector(tasksSelector);
-  const [filterTasks, setFilterTasks] = useState(tasks);
   const inputEl = useRef();
-  const debounce = useDebounce();
+  const [searchText, setSearchText] = useState('');
+  const filteredTasks = useMemo(() => {
+    if (!searchText) {
+      return tasks;
+    }
+    return tasks.filter(v => v.title.includes(searchText));
+  }, [tasks, searchText]);
+
 
   const onSearch = (evt) => {
-    const value = evt.target.value;
-    if (value === '') {
-      setFilterTasks(tasks);
-      return;
-    }
-    debounce(() => setFilterTasks(tasks.filter(v => v.title.includes(value))))();
+    setSearchText(evt.target.value);
   };
 
   useEffect(() => {
@@ -27,7 +27,7 @@ const AllTasks = () => {
   return (
     <div>
       <FormControl type="text" placeholder="Search" className="mr-sm-2" onChange={onSearch} ref={inputEl} />
-      <TasksView tasks={filterTasks} />
+      <TasksView tasks={filteredTasks} />
     </div>
   );
 };
