0ccc1b329c3ad2067e9f1f9716b7b06c2d493f82
Thao <thaoho21294@gmail.com>
2022-06-19 15:41:48 +0700

    TaskList: Fix unit test

--- client/components/__test__/TaskList.spec.js
+++ client/components/__test__/TaskList.spec.js
@@ -4,18 +4,22 @@ import { TASK_STATUS } from '../../constant'
 import { render, fireEvent, defaultStore } from '../../__mocks__/test-utils'
 import { addTask } from '../../redux/task/task.actions'
 
-test('deleting a task reduce amount of tasks in store', () => {
-  const tasks = [
-    { id: '01', title: 'task 01', status: TASK_STATUS.TODO },
-    { id: '02', title: 'task 02', status: TASK_STATUS.TODO },
-  ]
-  defaultStore.dispatch(addTask(tasks[0]))
-  defaultStore.dispatch(addTask(tasks[1]))
+describe('TaskList', () => {
+  test('deleting a task reduce amount of tasks in store', () => {
+    const tasks = [
+      { id: '01', title: 'task 01', status: TASK_STATUS.TODO },
+      { id: '02', title: 'task 02', status: TASK_STATUS.TODO },
+    ]
 
-  const { getByText } = render(<TaskList tasks={tasks} />)
-  fireEvent.mouseEnter(getByText('task 01'))
-  fireEvent.click(getByText('Delete'))
-  fireEvent.click(getByText('OK'))
+    const { getByText, store } = render(<TaskList tasks={tasks} />, {
+      initialState: {
+        tasks,
+      },
+    })
+    fireEvent.mouseEnter(getByText('task 01'))
+    fireEvent.click(getByText('Delete'))
+    fireEvent.click(getByText('OK'))
 
-  expect(defaultStore.getState().tasks.length).toBe(1)
+    expect(store.getState().tasks.length).toBe(1)
+  })
 })
